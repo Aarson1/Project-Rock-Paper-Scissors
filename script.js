209d1dee6ec897a894548801be7429d991d99e95
aarson <Aarson@live.fr>
2022-08-31 14:38:05 +0800

    Add play round

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function getComputerChoice() {
-    let possibleChoice = ["rock", "paper", "scissors"];
+    let possibleChoice = ['rock', 'paper', 'scissors'];
     let randChoice = possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
     console.log(randChoice);
+    return randChoice
 }
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        alert('Equality');
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
+        alert(`You won! ${playerSelection} beats ${computerSelection}`)
+    } else  {
+        alert(`You loose! ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+
+
+const playerSelection = prompt("What will you be playing?")
+const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection)
 
