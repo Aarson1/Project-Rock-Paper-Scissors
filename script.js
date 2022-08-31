73f5c0d24408c2a1b80cfa34896fd0c946fa2412
aarson <Aarson@live.fr>
2022-08-31 21:33:08 +0800

    test

--- script.js
+++ script.js
@@ -6,20 +6,39 @@ function getComputerChoice() {
 }
 
 
-
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection == computerSelection) {
+        roundResult = false
         alert('Equality');
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
+        roundResult = true
         alert(`You won! ${playerSelection} beats ${computerSelection}`)
     } else  {
+        roundResult = false
         alert(`You loose! ${computerSelection} beats ${playerSelection}`)
     }
+    return roundResult
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice()
+        let fiveprompt = prompt("What will you be playing?")
+        playRound(fiveprompt, computerSelection)
+        
+    }
+    if (roundResult == true) {
+        alert('you won all the round')
+    }
+    else {
+        alert('you loose all')
+    }
+} 
 
+let playerSelection = prompt("What will you be playing?")
 
-const playerSelection = prompt("What will you be playing?")
 const computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection)
+/*console.log(playRound(playerSelection, computerSelection));*/
+game()
 
