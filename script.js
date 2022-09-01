56f006e7722c79b3b8f7442c56c76a0758794485
aarson <Aarson@live.fr>
2022-09-01 17:27:19 +0800

    Correct spelling errors and variable names

--- script.js
+++ script.js
@@ -10,51 +10,48 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
         
-        alert('Equality');
-        return roundResult = 'equality'
+        alert('Draw.');
+        return roundResult = 'draw'
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
         
-        alert(`You won! ${playerSelection} beats ${computerSelection}`)
+        alert(`You won! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`)
         return roundResult = 'win'
     } else  {
         
-        alert(`You loose! ${computerSelection} beats ${playerSelection}`)
-        return roundResult = 'loose'
+        alert(`You lost! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`)
+        return roundResult = 'lose'
     }
     /*return roundResult*/
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let fiveprompt = prompt("What will you be playing?").toLowerCase();
-        let computerSelection2 = getComputerChoice();
-        playRound(fiveprompt, computerSelection2)
+        let loopPrompt = prompt("What will you be playing?").toLowerCase();
+        let computerSelectionLoop = getComputerChoice();
+        playRound(loopPrompt, computerSelectionLoop)
         if (roundResult === 'win') {
             playerScore++;
-        } else if (roundResult === 'loose') {
+        } else if (roundResult === 'lose') {
             computerScore++;
         }
 
         getComputerChoice()
-        console.log(computerSelection2)
+        console.log(computerSelectionLoop)
         console.log(playerScore, " for player")
         console.log(computerScore, " for computer")
 
     }
     if (playerScore == computerScore) {
-        alert('No one won.')
+        alert('It\'s a draw, no one won.')
     } else if (playerScore > computerScore) {
         alert('Congratulations! Yon won all round.')
     } else {
-        alert('Unfortunately, you loose all round.')
+        alert('Unfortunately, you lost all round.')
     }
 } 
 
 var playerScore = 0
 var computerScore = 0
-
-
 const computerSelection = getComputerChoice();
-/*console.log(playRound(playerSelection, computerSelection));*/
 game()
 
