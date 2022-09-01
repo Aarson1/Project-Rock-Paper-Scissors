a55fc112fc08625fac6ca7775145fdd8cd6212ec
aarson <Aarson@live.fr>
2022-09-01 15:31:43 +0800

    Adjust function game to play 5 round correctly

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     let possibleChoice = ['rock', 'paper', 'scissors'];
     let randChoice = possibleChoice[Math.floor(Math.random() * possibleChoice.length)];
-    console.log(randChoice);
+    /*console.log(randChoice)*/
     return randChoice
 }
 
@@ -9,34 +9,39 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
-        roundResult = false
+        
         alert('Equality');
+        return roundResult = 0
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
-        roundResult = true
+        
         alert(`You won! ${playerSelection} beats ${computerSelection}`)
+        return roundResult = 1
     } else  {
-        roundResult = false
+        
         alert(`You loose! ${computerSelection} beats ${playerSelection}`)
+        return roundResult = 0
     }
-    return roundResult
+    /*return roundResult*/
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        getComputerChoice()
         let fiveprompt = prompt("What will you be playing?")
-        playRound(fiveprompt, computerSelection)
+        let computerSelection2 = getComputerChoice();
+        playRound(fiveprompt, computerSelection2)
+        getComputerChoice()
+        console.log(computerSelection2)
+
         
     }
-    if (roundResult == true) {
+    /*if (roundResult == true) {
         alert('you won all the round')
     }
     else {
         alert('you loose all')
-    }
+    }*/
 } 
 
-let playerSelection = prompt("What will you be playing?")
 
 const computerSelection = getComputerChoice();
 /*console.log(playRound(playerSelection, computerSelection));*/
