7dd8182f2db3bb0e2f04f1e529302ca57fc8c714
aarson <Aarson@live.fr>
2022-09-01 17:10:19 +0800

    Display result of winner in all round

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         
         alert('Equality');
-        return roundResult = false
+        return roundResult = 'equality'
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
         
         alert(`You won! ${playerSelection} beats ${computerSelection}`)
-        return roundResult = true
+        return roundResult = 'win'
     } else  {
         
         alert(`You loose! ${computerSelection} beats ${playerSelection}`)
-        return roundResult = false
+        return roundResult = 'loose'
     }
     /*return roundResult*/
 }
@@ -29,18 +29,29 @@ function game() {
         let fiveprompt = prompt("What will you be playing?")
         let computerSelection2 = getComputerChoice();
         playRound(fiveprompt, computerSelection2)
-        if (roundResult) {
-            score++;
+        if (roundResult === 'win') {
+            playerScore++;
+        } else if (roundResult === 'loose') {
+            computerScore++;
         }
+
         getComputerChoice()
         console.log(computerSelection2)
-        console.log(score)
+        console.log(playerScore, " for player")
+        console.log(computerScore, " for computer")
 
     }
-    
+    if (playerScore == computerScore) {
+        alert('No one won.')
+    } else if (playerScore > computerScore) {
+        alert('Congratulations! Yon won all round.')
+    } else {
+        alert('Unfortunately, you loose all round.')
+    }
 } 
 
-var score = 0
+var playerScore = 0
+var computerScore = 0
 
 
 const computerSelection = getComputerChoice();
